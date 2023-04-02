d05ea0340c5d67164bc502c6af0da883cfe60acd
Security-Shark <127664943+Security-Shark@users.noreply.github.com>
2023-04-02 11:58:13 +0530

    Add files via upload

---
+++ rot13.py
@@ -0,0 +1,25 @@
+
+
+
+
+my_d2={1:"A",2:"B",3:"C",..............}
+my_d1={"A":1, "B":2, "C":3,................"Z":26}
+
+pt=input("Enter the PT to encryprt: ")
+
+def encryptttt(pt):
+	
+	for alpha in pt:
+		num=my_d1[alpha] + 13
+		ct=my_d2[num]
+	asdfasd
+	asdasd
+	asdasdas
+	return ct
+	
+def decrytptt(ct):
+	wdasd
+	asdasd
+	asdasd
+	asdada
+	return pt
